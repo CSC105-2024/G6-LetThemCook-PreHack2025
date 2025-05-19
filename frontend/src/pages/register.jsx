390c515c7f656d5fe9a0d9b5332d114849e4e34f
NichapaJongKmutt <nichapa.jong@mail.kmutt.ac.th>
2025-05-19 12:20:07 +0700

    bro

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 function Register() {
+      const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
+  const openEye = <FontAwesomeIcon icon={faEye} />;
+  const closeEye = <FontAwesomeIcon icon={faEyeSlash} />;
   return (
     <>
       <div className="page-container ">
@@ -31,6 +36,40 @@ function Register() {
                         />
                         <div className="bg-black w-full h-[1px]"></div>
                     </div>
+                    <div className="input-group  flex flex-col gap-2 w-100%">
+              <label className="text-2xl">Password</label>
+              <div className="flex gap-3 items-center justify-between ">
+                <input
+                  className="px-4 py-3 w-[100%]"
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="e.g weresocooked@gmail.com"
+                />
+                <span>
+                  <button onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? openEye : closeEye}
+                  </button>
+                </span>
+              </div>
+              <div className="bg-black w-full h-[1px]"></div>
+            </div>
+            <div className="input-group  flex flex-col gap-2 w-100%">
+              <label className="text-2xl">Password</label>
+              <div className="flex gap-3 items-center justify-between ">
+                <input
+                  className="px-4 py-3 w-[100%]"
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="e.g weresocooked@gmail.com"
+                />
+                <span>
+                  <button onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? openEye : closeEye}
+                  </button>
+                </span>
+              </div>
+              <div className="bg-black w-full h-[1px]"></div>
+            </div>
                     
                 </div>
                 <div className="sign-btn">
