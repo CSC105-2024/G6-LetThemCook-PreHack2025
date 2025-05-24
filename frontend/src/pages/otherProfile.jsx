5c63aed4db73aa7c26a933d3e8f3daecfe56fe4f
Seamew <yutsuzetime@gmail.com>
2025-05-25 03:31:26 +0700

    Update otherProfile.jsx

--- frontend/src/pages/otherProfile.jsx
+++ frontend/src/pages/otherProfile.jsx
@@ -9,7 +9,7 @@ function OtherProfilePage() {
     const [bio, setBio] = useState('hold up, let me cook.');
 
     return (
-        <div className="min-h-screen bg-[#E9E5DC] flex items-center justify-center ">
+        <div className="min-h-screen bg-[#E9E5DC] flex items-center justify-center px-4 ">
             <div className="w-full max-w-sm md:max-w-xl lg:max-w-2xl bg-white p-5 sm:p-8 rounded-lg shadow-md">
                 <div className="md:max-w-sm lg:max-w-md xl:max-w-lg mx-auto items-center ">
                     <h1 className="text-black font-semibold text-lg  mb-9">{username}'s Profile</h1>
