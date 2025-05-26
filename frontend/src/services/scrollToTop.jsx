b5925c0b171dcdd49fd348dcefa442f7386cf96c
NichapaJongKmutt <nichapa.jong@mail.kmutt.ac.th>
2025-05-26 12:27:05 +0700

    route

--- frontend/src/services/scrollToTop.jsx
+++ frontend/src/services/scrollToTop.jsx
@@ -1,13 +1,14 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-function ScrollToTop(){
-    const {pathname} = useLocation();
-    useEffect(()=>{
-        window.scrollTo({
-            top:0
-        })
-    },[pathname])
-    return null;
-}
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 export default ScrollToTop;
